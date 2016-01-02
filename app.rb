6f1ec9c2584d62155e58d1ae226e6f328687ef46
Paula Murphy <paulam849@gmail.com>
2016-01-02 16:26:15 -0500

    added post page

--- app.rb
+++ app.rb
@@ -6,9 +6,15 @@ require "./models"
 set :database, "sqlite3:blogdb.sqlite3"
 
 get '/' do
+	Post.create(title:"Dog Park", body: "This is a post")
 	@posts = Post.all
 	erb :home
 end	
+
+get '/post' do
+	@last_post = Post.last
+	erb :post_page
+end
 
 get '/sign_in' do
 	erb :signin	
